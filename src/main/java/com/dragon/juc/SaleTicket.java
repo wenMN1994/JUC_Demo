0f13c0f961fdc3f806274e8b767eee2aed1558f2
温文星 <18475536452@163.com>
2020-01-20 16:42:05 +0800

    Lock接口的实现 ReentrantLock可重入锁

--- src/main/java/com/dragon/juc/SaleTicket.java
+++ src/main/java/com/dragon/juc/SaleTicket.java
@@ -1,5 +1,8 @@
 package com.dragon.juc;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author：Dragon Wen
  * @email：18475536452@163.com
@@ -12,7 +15,10 @@ class Ticket{
     //创建资源
     private int number = 30;
 
-    public synchronized void sale(){
+    /**
+     * 复习synchronized
+     */
+    /*public synchronized void sale(){
         synchronized(this){
 
         }
@@ -21,6 +27,25 @@ class Ticket{
             System.out.println(Thread.currentThread().getName()
                     +"\t 卖出"+number--+"号票\t还剩"+number
             );
+        }
+    }*/
+
+    //Lock接口的实现
+    //ReentrantLock可重入锁
+    Lock lock = new ReentrantLock();
+
+    public void sale(){
+        lock.lock();
+        try {
+            if(number > 0){
+                System.out.println(Thread.currentThread().getName()
+                        + "\t 卖出" + number-- + "号票\t还剩" + number
+                );
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
         }
     }
 }
@@ -33,13 +58,31 @@ public class SaleTicket {
      * @param args
      * @throws Exception 线程 操作 资源类,高内聚低耦合
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         //实例化一个资源对象
         Ticket ticket = new Ticket();
 
+        //通过lambda表达式
+        new Thread(()->{
+            for (int i = 1; i < 40; i++) {
+                ticket.sale();
+            }
+        },"AA").start();
+
+        new Thread(()->{
+            for (int i = 1; i < 40; i++) {
+                ticket.sale();
+            }
+        },"BB").start();
+
+        new Thread(()->{
+            for (int i = 1; i < 40; i++) {
+                ticket.sale();
+            }
+        },"CC").start();
         //匿名内部类方式创建线程对象
-        new Thread(new Runnable() {
+        /*new Thread(new Runnable() {
             @Override
             public void run() {
                 for (int i = 1; i < 40; i++) {
@@ -64,6 +107,6 @@ public class SaleTicket {
                     ticket.sale();
                 }
             }
-        },"CC").start();
+        },"CC").start();*/
     }
 }
