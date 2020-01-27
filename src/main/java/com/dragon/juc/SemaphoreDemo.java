ae36fa094e82d42e56c8f217646419686ffadbec
温文星 <18475536452@163.com>
2020-01-27 21:53:13 +0800

    Semaphore 信号灯

---
+++ src/main/java/com/dragon/juc/SemaphoreDemo.java
@@ -0,0 +1,44 @@
+package com.dragon.juc;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author：Dragon Wen
+ * @email：18475536452@163.com
+ * @date：Created in 2020/1/27 21:47
+ * @description：Semaphore 信号灯
+ * @modified By：
+ * @version: $
+ * 在信号量上我们定义两种操作：
+ * acquire（获取） 当一个线程调用acquire操作时，它要么通过成功获取信号量（信号量减1），
+ * 要么一直等下去，直到有线程释放信号量，或超时。
+ * release（释放）实际上会将信号量的值加1，然后唤醒等待的线程。
+ * 信号量主要用于两个目的，一个是用于多个共享资源的互斥使用，另一个用于并发线程数的控制。
+ */
+public class SemaphoreDemo {
+
+    public static void main(String[] args) {
+
+        //定义三个停车位
+        Semaphore semaphore = new Semaphore(3);
+
+        //停六辆汽车
+        for (int i = 1; i <= 6; i++) {
+            new Thread(()->{
+                try {
+                    semaphore.acquire();
+                    System.out.println(Thread.currentThread().getName()
+                            +"\t号车驶入停车位");
+                    TimeUnit.SECONDS.sleep(3);
+                    System.out.println(Thread.currentThread().getName()
+                            +"\t号车驶出停车位");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    semaphore.release();
+                }
+            },String.valueOf(i)).start();
+        }
+    }
+}
