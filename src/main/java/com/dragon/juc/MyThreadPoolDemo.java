87ae9c91748bf3da272b554bd424d1f6c0f975e6
温文星 <18475536452@163.com>
2020-02-03 22:58:48 +0800

    线程池7个重要参数

--- src/main/java/com/dragon/juc/MyThreadPoolDemo.java
+++ src/main/java/com/dragon/juc/MyThreadPoolDemo.java
@@ -1,7 +1,6 @@
 package com.dragon.juc;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 /**
  * @author：Dragon Wen
@@ -14,6 +13,33 @@ import java.util.concurrent.Executors;
 public class MyThreadPoolDemo {
 
     public static void main(String[] args) {
+        ExecutorService threadPool = new ThreadPoolExecutor(
+                2,
+                5,
+                3L,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<Runnable>(3),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.DiscardPolicy()
+                //new ThreadPoolExecutor.DiscardOldestPolicy()
+                //new ThreadPoolExecutor.CallerRunsPolicy()
+                //new ThreadPoolExecutor.AbortPolicy()
+        );
+        try {
+            for (int i = 1; i <=10 ; i++) {
+                threadPool.execute(()->{
+                    System.out.println(Thread.currentThread().getName()
+                            +"\t 号业务员办理业务");
+                });
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            threadPool.shutdown();
+        }
+    }
+
+    private static void ThreadPool() {
         ExecutorService threadPool1 = Executors.newFixedThreadPool(3);// 银行网点3个窗口
         ExecutorService threadPool2 = Executors.newSingleThreadExecutor();// 银行网点1个窗口
         ExecutorService threadPool3 = Executors.newCachedThreadPool();// 银行网点可扩展窗口
