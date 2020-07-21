f1f94ec6d96b91d9e0026aa2e27b5ab3b03cbf7f
DragonWen <18475536452@163.com>
2020-07-21 17:52:47 +0800

    死锁

---
+++ src/main/java/com/dragon/juc/interview/DeadLockDemo.java
@@ -0,0 +1,47 @@
+package com.dragon.juc.interview;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author：Dragon Wen
+ * @email：18475536452@163.com
+ * @date：Created in 2020/7/21 16:03
+ * @description：死锁
+ * @modified By：
+ * @version: $
+ */
+class HoldThread implements Runnable {
+
+    private String lockA;
+    private String lockB;
+
+    public HoldThread(String lockA, String lockB) {
+        this.lockA = lockA;
+        this.lockB = lockB;
+    }
+
+    @Override
+    public void run() {
+        synchronized (lockA) {
+            System.out.println(Thread.currentThread().getName() + "\t 自己持有锁" + lockA + "尝试获得" + lockB);
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            synchronized (lockB) {
+                System.out.println(Thread.currentThread().getName() + "\t 自己持有锁" + lockB + "尝试获得" + lockA);
+            }
+        }
+    }
+}
+
+public class DeadLockDemo {
+    public static void main(String[] args) {
+        String lockA = "lockA";
+        String lockB = "lockB";
+        new Thread(new HoldThread(lockA, lockB), "threadAAA").start();
+        new Thread(new HoldThread(lockB, lockA), "threadBBB").start();
+    }
+
+}
